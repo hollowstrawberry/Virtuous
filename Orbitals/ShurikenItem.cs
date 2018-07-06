2e28a25dfaa15e1c84439fe24f8f1b7f2c721479
もゃゅ语ゑふ <37935150+Dierney@users.noreply.github.com>
2018-07-06 16:42:00 +0800

    Little fix

--- Orbitals/ShurikenItem.cs
+++ Orbitals/ShurikenItem.cs
@@ -9,7 +9,7 @@ namespace Virtuous.Orbitals
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Twilight");
-			Tooltip.SetDefault(
+            Tooltip.SetDefault(
                 "Shuriken defend you, raising melee speed and mana regeneration\nAligns with either magic or melee users");
 
             DisplayName.AddTranslation(GameCulture.Spanish, "Crepúsculo");
@@ -22,7 +22,7 @@ namespace Virtuous.Orbitals
                 "Сюрикены защищают вас, увеличивая скорость ближнего боя и регенерации маны\nПодходит воинам и магам");
 
             DisplayName.AddTranslation(GameCulture.Chinese, "暮色旋镖");
-			Tooltip.AddTranslation(GameCulture.Chinese,
+            Tooltip.AddTranslation(GameCulture.Chinese,
                 "暮色旋镖将保护你,提高近战攻速及法力再生\n" +
                 "更适合战士与法师使用");
         }
